fc59d06cdd8cd52d31c9daeb4506f071727b1af4
kush46 <97089328+kushal46@users.noreply.github.com>
2022-02-03 10:04:46 +0000

    0

---
+++ p3final.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int input_n()
+{
+  int n;
+  printf("Enter The Number\n");
+  scanf("%d",&n);
+  return n;
+}
+
+
+int sum_n(int n)
+{
+  int sum=0,i;
+
+  for (i=1; i<=n; i++)
+  {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+void output(int n,int sum)
+{
+printf("The sum of %d numbers is %d\n",n,sum);
+}
+
+int main()
+{ int n,sum;
+  n = input_n();
+  sum = sum_n(n);
+  output(n,sum);
+  return 0;
+}
--- p3original.c
+++ p3original.c
@@ -13,7 +13,7 @@ int sum_n(int n)
 {
   int sum=0,i;
 
-  for (i=0; i<n; i++)
+  for (i=1; i<=n; i++)
   {
     sum = sum + i;
   }
